80836485888b1b74f63fceb8f0f9cbb5fbfc6208
DEM <dmccollum@westmont.edu>
2023-02-13 08:59:05 -0800

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  //let result = string.repeat(num);
+  //return result;
+  if (num > 0) {
+    string.repeat(num);
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

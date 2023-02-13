0d15eff194dde0f600d80d3df01d6a9ace7c3c68
DEM <dmccollum@westmont.edu>
2023-02-13 14:53:03 -0800

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  let result = array.filter((arg) => !args.includes(arg));
+  return result;
 };
 
 // Do not edit below this line

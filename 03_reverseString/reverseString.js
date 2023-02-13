4b41180e52bb659cdef275f6b5ee5cff31892137
DEM <dmccollum@westmont.edu>
2023-02-13 14:03:27 -0800

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  //splits string into an array of individual letters
+  let toArray = input.split("");
+  //reverses the array
+  let reverse = toArray.reverse();
+  //joins the reversed array back into a string
+  let result = reverse.join("");
+  //displays the result
+  return result;
 };
 
 // Do not edit below this line

620a9ffe3522e53505d1b1ba72fd8128c772c8cf
Danthecodeman <dmccollum@westmont.edu>
2023-05-19 15:20:07 -0700

    DM - 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // Remove punctuation and convert to lowercase
+  const cleanStr = string.replace(/[^\w]/g, "").toLowerCase();
+  // Reverse the string
+  const reversedStr = cleanStr.split("").reverse().join("");
+  // Check if the reversed string is equal to the original string
+  return cleanStr === reversedStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;

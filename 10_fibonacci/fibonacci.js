058c39caf8445dbbebefcac760d8a769bd647dd8
Danthecodeman <dmccollum@westmont.edu>
2023-05-23 11:47:06 -0700

    DM - 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  // Check if the input is a non-negative integer
+  if (n < 0) {
+    return "OOPS";
+  }
 
+  if (n === 0) {
+    return 0;
+  }
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return b;
 };
 
 // Do not edit below this line

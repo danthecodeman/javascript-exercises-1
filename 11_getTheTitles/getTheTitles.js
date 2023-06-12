314b8ebb2cbebfad6e4eae0701350d9b2922a79d
Danthecodeman <dmccollum@westmont.edu>
2023-06-12 14:24:13 -0700

    DM - Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  //make a function that returns an array of the titles
 
+  return books.map((book) => book.title);
 };
 
 // Do not edit below this line

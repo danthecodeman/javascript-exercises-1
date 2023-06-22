1f268ceb67efcb3355dd84faaa46f6f94525ae02
Danthecodeman <dmccollum@westmont.edu>
2023-06-22 14:02:19 -0700

    DM - Exercise find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  const oldestPerson = people.reduce((oldest, person) => {
+    const personAge = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : currentYear - person.yearOfBirth;
+    const oldestPersonAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : currentYear - oldest.yearOfBirth;
+
+    return personAge > oldestPersonAge ? person : oldest;
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
